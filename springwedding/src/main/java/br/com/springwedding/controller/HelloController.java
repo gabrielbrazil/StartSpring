546fa9070c6b117a88562ce9221e22ac5f37056a
gabrielSousa <gabrielbrazil@hotmail.com>
2015-04-21 17:33:19 -0300

    first commit

---
+++ springwedding/src/main/java/br/com/springwedding/controller/HelloController.java
@@ -0,0 +1,36 @@
+package br.com.springwedding.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloController {
+
+	private String nome;
+	
+	@RequestMapping("/olaMundoSpring")
+	public String execute(){
+		System.out.println("Testet");
+		return "hello";
+	}
+	
+	@RequestMapping("/sayHelloWorld")
+	public String sayHello(Model model,String nome){
+		model.addAttribute("nome",nome);
+		return "hello2";
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	};
+	
+	
+}
+
+
---
+++ springwedding/src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,9 @@
+<html>
+	<body>
+		<form action="sayHelloWorld" method="post">
+			<label>Nome:</label>
+			<input type="text" name="nome"/>
+			<input type="submit" value="enviar" />
+		</form>
+	</body>
+</html>
