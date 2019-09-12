c984735fb079341e9d4122576ec18c5bf95ea1c9
gabriel_sousa <gabriel.proxy.brazil@gmail.com>
2019-09-12 16:50:15 -0300

    Repo 1

--- springwedding/src/main/java/br/com/springwedding/controller/HelloController.java
+++ springwedding/src/main/java/br/com/springwedding/controller/HelloController.java
@@ -19,7 +19,7 @@ public class HelloController {
 	@RequestMapping("/sayHelloWorld")
 	public String sayHello(Model model,String nome){
 		model.addAttribute("nome",nome);
-		return "hello34";
+		return "helloRepo1";
 	}
 
 	public String getNome() {
