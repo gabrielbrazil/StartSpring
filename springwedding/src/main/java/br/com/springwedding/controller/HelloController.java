947d50ef10c8451292dca14eea4932e82a402cb0
gabriel_sousa <gabriel.proxy.brazil@gmail.com>
2019-09-12 12:10:46 -0300

    teste 2

--- springwedding/src/main/java/br/com/springwedding/controller/HelloController.java
+++ springwedding/src/main/java/br/com/springwedding/controller/HelloController.java
@@ -19,7 +19,7 @@ public class HelloController {
 	@RequestMapping("/sayHelloWorld")
 	public String sayHello(Model model,String nome){
 		model.addAttribute("nome",nome);
-		return "hello3";
+		return "hello34";
 	}
 
 	public String getNome() {
