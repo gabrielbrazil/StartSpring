ba96d8065729a926654e87527bb5a1286db4f372
gabriel_sousa <gabriel.proxy.brazil@gmail.com>
2019-09-12 12:16:51 -0300

    cenario teste de merge

--- springwedding/src/main/java/br/com/springwedding/controller/HelloController.java
+++ springwedding/src/main/java/br/com/springwedding/controller/HelloController.java
@@ -19,7 +19,7 @@ public class HelloController {
 	@RequestMapping("/sayHelloWorld")
 	public String sayHello(Model model,String nome){
 		model.addAttribute("nome",nome);
-		return "hello2";
+		return "hello25";
 	}
 
 	public String getNome() {
