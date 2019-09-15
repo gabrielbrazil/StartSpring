16df128563730b806898d4a65fdc64ffe1db96b8
gabriel_sousa <gabriel.proxy.brazil@gmail.com>
2019-09-15 11:56:07 -0300

    rebase repo1

--- springwedding/src/main/java/br/com/springwedding/controller/HelloController.java
+++ springwedding/src/main/java/br/com/springwedding/controller/HelloController.java
@@ -19,7 +19,7 @@ public class HelloController {
 	@RequestMapping("/sayHelloWorld")
 	public String sayHello(Model model,String nome){
 		model.addAttribute("nome",nome);
-		return "helloRepo2Rebase";
+		return "helloRepo1Rebase";
 	}
 
 	public String getNome() {
