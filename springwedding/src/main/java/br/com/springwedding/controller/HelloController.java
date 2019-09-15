3fb15f8505a1967eb72a7909c0b16cb962f05cd9
gabriel_sousa <gabriel.proxy.brazil@gmail.com>
2019-09-15 14:21:16 -0300

    target rebase test

--- springwedding/src/main/java/br/com/springwedding/controller/HelloController.java
+++ springwedding/src/main/java/br/com/springwedding/controller/HelloController.java
@@ -19,7 +19,7 @@ public class HelloController {
 	@RequestMapping("/sayHelloWorld")
 	public String sayHello(Model model,String nome){
 		model.addAttribute("nome",nome);
-		return "changedRebase1223";
+		return "rebase";
 	}
 
 	public String getNome() {
